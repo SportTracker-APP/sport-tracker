1534522e76524b68afbd5bd944bf3e6b0750d1b2
Thibaut ROSTAN <sport.tracker.pro@gmail.com>
2026-05-19 16:30:36 +0200

    Setup backend base: server, env, dependencies #2

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware (outils qui traitent les requêtes)
+app.use(cors());
+app.use(express.json());
+
+// Connexion MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connecté");
+
+    // Lancement du serveur
+    app.listen(process.env.PORT, () => {
+      console.log(`Serveur lancé sur le port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Erreur MongoDB :", err);
+  });
